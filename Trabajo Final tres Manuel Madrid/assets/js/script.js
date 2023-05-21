5ff1defad3ca376edef73f8aaeb2b7051a07bec8
Manuel Madrid <119515892+ManuelMadridVal@users.noreply.github.com>
2023-05-21 13:03:32 -0400

    Add files via upload
    Trabajo Final tres Manuel Madrid

---
+++ Trabajo Final tres Manuel Madrid/assets/js/script.js
@@ -0,0 +1,13 @@
+var miTexto = document.getElementById("miTexto");
+
+miTexto.addEventListener("click", function () {
+  if (miTexto.style.fontWeight === "bold") {
+    miTexto.style.fontWeight = "normal";
+  } else {
+    miTexto.style.fontWeight = "bold";
+  }
+});
+
+object.onmousemove = function () {
+  myScript;
+};
